6fd754ce8b8432c3f48f423989eb2163e1da728f
Christian Scholtysik <christianscholtysik@gmail.com>
2024-07-12 22:46:37 +0200

    change blog images

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -12,26 +12,18 @@ const Blog = () => {
       <div className="blog-wrapper">
         <div className="img-wrapper">
           <div className="blog-img">
-            <img src="/public/Image.png" alt="" className="blog-image" />
+            <img src="/Image (2).svg" alt="" className="blog-image" />
             <p></p>
             <h5></h5>
             <p></p>
-            <img
-              src="/public/Sub Container.png"
-              alt=""
-              className="bottom-img"
-            />
+            <img src="/Sub Container.svg" alt="" className="bottom-img" />
           </div>
           <div className="blog-img2">
-            <img src="/public/Image (1).png" alt="" className="blog-image" />
+            <img src="/Image (1).svg" alt="" className="blog-image" />
             <p></p>
             <h5></h5>
             <p></p>
-            <img
-              src="/public/Sub Container (1).png"
-              alt=""
-              className="bottom-img"
-            />
+            <img src="/Sub Container (1).svg" alt="" className="bottom-img" />
           </div>
         </div>
       </div>
